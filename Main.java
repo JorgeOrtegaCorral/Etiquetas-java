99458c5b111ead93fc86291649e8700a3efe265b
victor <victortwt6@gmail.com>
2024-10-24 07:42:23 +0000

    argumentos

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args){
+		if(args.length == 0){
+			System.out.println("No hay argumentos");
+		}else{
+			System.out.println("Hay" + args.length + "argumentos");
+		}
+
+	}
+
+}
